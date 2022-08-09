b2747a6a26c7e450cacdc97955dcfa6f5a24c6cf
fprompto <fernando.monteiro.f11@gmail.com>
2022-08-09 09:31:07 -0300

    :sparkles: feat: Página de perfil adicionada

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,17 @@
 import Home from '../pages/Home';
+import Profile from '../pages/Profile';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     element: Home,
+    exact: true,
+  },
+  {
+    path: '/user/:user',
+    name: 'Perfil',
+    element: Profile,
     exact: true,
   },
 ];
